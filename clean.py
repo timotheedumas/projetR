d02068742ba45cbf2afbd47e35a6f9f64f4df19f
yannistml <yannis.tumelin@edu.esiee.fr>
2023-11-09 13:26:13 +0100

    ajout des données nettoyées et du script utilisé pour les nettoyer

---
+++ clean.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("ufo-sightings-transformed.csv")
+
+df2 = df.dropna(axis=0)
+
+df2["length_of_encounter_seconds"] = df2["length_of_encounter_seconds"].astype(float)
+df2["length_of_encounter_minuts"] = (df2["length_of_encounter_seconds"] / 60).astype(np.int64)
+
+# Enregistrement du DataFrame nettoyé dans un nouveau fichier CSV
+df2.to_csv("clean-ufo-sightings-transformed.csv", index=False)
