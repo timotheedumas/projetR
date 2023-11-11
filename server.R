0be789204f6788c9da4d91f1f7c4447199feb746
yannistml <yannis.tumelin@edu.esiee.fr>
2023-11-11 18:19:39 +0100

    ajout des onglets et des messsages personnalisés

--- server.R
+++ server.R
@@ -3,64 +3,167 @@ library(dplyr)
 library(leaflet)
 library(scales)
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
+
+server <- function(input, output, session) {
+  #load the datas
   data_set_ufo <-
     read.csv("clean-ufo-sightings-transformed.csv",
              sep = ",",
              encoding = "UTF-8")
   
   
+  #filter the  year based on user input
   selectedYearHistogram <- reactive({
-    data_set_ufo %>% filter(Year == input$years) %>%
+    data_set_ufo %>% filter(Year == input$yearHistogram) %>%
       mutate(
         Duration_Category = cut(
           length_of_encounter_seconds,
           breaks = c(-1, 10, 60, 300, 1800, 7200, 86400, Inf),
-          labels = c('< 10 sec', '10 sec - 1 min', '1 - 5 min', '5 - 30 min', '30 min - 2h', '2h - 1 day', '> 1 day'),
+          labels = c(
+            '< 10 sec',
+            '10 sec - 1 min',
+            '1 - 5 min',
+            '5 - 30 min',
+            '30 min - 2h',
+            '2h - 1 day',
+            '> 1 day'
+          ),
           include.lowest = TRUE
         )
       )
   })
+  #display an error message if there is no data available for the selected year
+  output$errorTextHistogram <- renderText({
+    selected_year <- input$yearHistogram
+    if (!selected_year %in% data_set_ufo$Year) {
+      return(paste("No data available for the year", selected_year))
+    }
+  })
   
+  #plot the histogram
   output$histogram <- renderPlot({
-    ggplot(selectedYearHistogram(), aes(x = Duration_Category, fill = Duration_Category)) +
+    ggplot(selectedYearHistogram(),
+           aes(x = Duration_Category, fill = Duration_Category)) +
       geom_bar() +
-      scale_fill_manual(values = c('< 10 sec' = 'blue', '10 sec - 1 min' = 'blue', '1 - 5 min' = 'blue', '5 - 30 min' = 'blue', '30 min - 2h' = 'blue', '2h - 1 day' = 'blue', '> 1 day' = 'blue')) +
+      scale_fill_manual(
+        values = c(
+          '< 10 sec' = '#3498db',
+          '10 sec - 1 min' = '#3498db',
+          '1 - 5 min' = '#3498db',
+          '5 - 30 min' = '#3498db',
+          '30 min - 2h' = '#3498db',
+          '2h - 1 day' = '#3498db',
+          '> 1 day' = '#3498db'
+        )
+      ) +
       labs(title = "Histogram of Encounter Duration Categories", x = "Duration Category", y = "Frequency")
   })
   
-  
-  selectedyearPie <- reactive({
-    data_set_ufo %>% filter(Year == input$year_for_pie)
+  #filter the  year based on user input
+  selectedYearPieSeason <- reactive({
+    data_set_ufo %>% filter(Year == input$year_for_pie_seasons)
   })
+  
+  #count the number of occurence for each season
   season_counts <- reactive({
-    table(selectedyearPie()$Season)
+    table(selectedYearPieSeason()$Season)
   })
   
-  custom_colors <- c("#f28500", "green", "#F2FD00", "#00FDF2")
   
-  output$pieChart <- renderPlot({
-    pie(
-      season_counts(),
-      labels = names(season_counts()),
-      main = paste(
-        "Pie Chart of the repartition of the encounters of UFO by season for the year",
-        input$year_for_pie
-      ),
-      col = custom_colors
-    )
+  
+  #plot the pie chart for seasons
+  output$pieChartSeasons <- renderPlot({
+    if (!is.null(season_counts()) && length(season_counts()) > 0) {
+      pie(
+        season_counts(),
+        labels = names(season_counts()),
+        main = paste(
+          "Pie Chart of the repartition of the encounters of UFO by season for the year",
+          input$year_for_pie_seasons
+        )
+      )
+    }
+    #display an error message if there is no relevant datas for the selected year
+    else {
+      plot(
+        1,
+        type = "n",
+        axes = FALSE,
+        xlab = "",
+        ylab = ""
+      )
+      text(
+        1,
+        1,
+        paste(
+          "No sufficient datas to plot a pie chart for the year",
+          input$year_for_pie_seasons
+        ),
+        cex = 1.2,
+        col = "black",
+        font = 2
+      )
+    }
+  })
+  
+  #filter the  year based on user input
+  selectedYearShapes <- reactive({
+    data_set_ufo %>% filter(Year == input$year_for_pie_shapes)
+  })
+  #count the number of occurence for each shape
+  shape_counts <- reactive({
+    table(selectedYearShapes()$UFO_shape)
+  })
+  
+  #plot the histogram for the shapes repartition
+  output$pieChartShapes <- renderPlot({
+    if (!is.null(shape_counts()) && length(shape_counts()) > 0) {
+      pie(
+        shape_counts(),
+        labels = names(shape_counts()),
+        main = paste(
+          "Pie Chart of the repartition of the UFO shape",
+          input$year_for_pie_shapes
+        )
+      )
+    }
+    else {
+      plot(
+        1,
+        type = "n",
+        axes = FALSE,
+        xlab = "",
+        ylab = ""
+      )
+      text(
+        1,
+        1,
+        paste(
+          "No sufficient datas to plot a pie chart for the year",
+          input$year_for_pie_shapes
+        ),
+        cex = 1.2,
+        col = "black",
+        font = 2
+      )
+    }
     
   })
   
-  
+  output$plotUfoEncounters <-
+    renderPlot({
+      ggplot(data_set_ufo, aes(x = Year)) +
+        geom_line(stat = "count") +
+        labs(title = "Total Number of UFO Encounters by Year", x = "Year", y = "Number of Encounters") +
+        theme_minimal()
+    })
   
   
   output$ufoMap <- renderLeaflet({
-    map_data <- data_set_ufo %>%
+    selectYearMap <- data_set_ufo %>%
       filter(Year == input$year_for_map)
     
-    leaflet(map_data) %>%
+    leaflet(selectYearMap) %>%
       addTiles() %>%
       addCircleMarkers(
         lng = ~ longitude,
@@ -73,9 +176,12 @@ server <- function(input, output) {
           sep = "<br>"
         ),
         radius = 5,
+        color = '#3498db',
         fillOpacity = 0.8,
         stroke = FALSE,
         options = popupOptions(closeButton = FALSE)
       )
+   
   })
+  
 }
--- ui.R
+++ ui.R
@@ -15,58 +15,75 @@ data_set_ufo <-
            sep = ",",
            encoding = "UTF-8")
 
-ui <- fluidPage(
-  # App title ----
-  titlePanel(h1(
-    "Dashboard to analyse the ufo sightings ", align = "center"
-  )),
-  
-  
-  mainPanel(
-    plotOutput("histogram"),
+ui <- fluidPage(titlePanel(h1(
+  "Dashboard to analyse the ufo sightings ", align = "center"
+)),
+
+
+
+mainPanel(
+  tabsetPanel(
+    tabPanel(
+      "Histogram",
+      plotOutput("histogram"),
+      sliderInput(
+        inputId = "yearHistogram",
+        label = "Year",
+        min = min(data_set_ufo$Year),
+        max = max(data_set_ufo$Year),
+        step = 1,
+        value = 1960,
+        sep = ""
+      )
+    ),
     
-    sliderInput(
-      inputId = "years",
-      label = "Year",
-      min = min(data_set_ufo$Year),
-      max = max(data_set_ufo$Year),
-      step = 1,
-      value = 1960,
-      sep = ""
+    tabPanel(
+      "Pie chart Seasons",
+      plotOutput("pieChartSeasons"),
+      sliderInput(
+        inputId = "year_for_pie_seasons",
+        label = "Year",
+        min = min(data_set_ufo$Year),
+        max = max(data_set_ufo$Year),
+        step = 1,
+        value = 1960,
+        sep = ""
+      )
+    ),
+    
+    tabPanel(
+      "Pie chart Shapes",
+      plotOutput("pieChartShapes"),
+      sliderInput(
+        inputId = "year_for_pie_shapes",
+        label = "Year",
+        min = min(data_set_ufo$Year),
+        max = max(data_set_ufo$Year),
+        step = 1,
+        value = 1960,
+        sep = ""
+      ),
+      
+    ),
+    
+    tabPanel(
+      "plot of the UFO encounters per year"
+      ,
+      plotOutput("plotUfoEncounters")
+    ),
+    
+    tabPanel(
+      "Map of the UFO encounters",
+      leafletOutput("ufoMap"),
+      sliderInput(
+        inputId = "year_for_map",
+        label = "Year",
+        min = min(data_set_ufo$Year),
+        max = max(data_set_ufo$Year),
+        step = 1,
+        value = 1960,
+        sep = ""
+      ),
     )
-  ),
-  
-  tags$br(),
-  
-  
-  mainPanel(
-    plotOutput("pieChart"),
-    sliderInput(
-      inputId = "year_for_pie",
-      label = "Year",
-      min = min(data_set_ufo$Year),
-      max = max(data_set_ufo$Year),
-      step = 1,
-      value = 1960,
-      sep = ""
-    )
-  ),
-  mainPanel(tabsetPanel(type = "tabs",
-                        
-                        tabPanel(
-                          "Map", leafletOutput("ufoMap")
-                        )),
-            sidebarPanel(
-                          sliderInput(
-                            inputId = "year_for_map",
-                            label = "Year",
-                            min = min(data_set_ufo$Year),
-                            max = max(data_set_ufo$Year),
-                            step = 1,
-                            value = 1960,
-                            sep = ""
-                          ))
-  
   )
-  
-)
+))
