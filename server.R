6aab0d3a3352d0aa4dfc18f6af128f7c7f3d97f3
yannistml <yannis.tumelin@edu.esiee.fr>
2023-11-11 13:10:56 +0100

    modification des sliders inputs

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(dplyr)
 library(leaflet)
+library(scales)
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
@@ -10,29 +11,31 @@ server <- function(input, output) {
              encoding = "UTF-8")
   
   
-  selectedYear <- reactive({
+  selectedYearHistogram <- reactive({
     data_set_ufo %>% filter(Year == input$years)
   })
   
   output$histogram <- renderPlot({
-    ggplot(selectedYear(),
+    ggplot(selectedYearHistogram(),
            aes(x = length_of_encounter_minutes, stat = "count")) +
       geom_histogram(
         bins = 20,
-        binwidth = max(selectedYear()$length_of_encounter_minutes) / 50,
+        binwidth = max(selectedYearHistogram()$length_of_encounter_minutes) / 50,
         fill = "blue",
         color = "black",
         alpha = 0.7
       ) +
       labs(title = "Histogram of Encounter Length", x = "Length (minutes)", y = "Frequency") + coord_cartesian(xlim = c(0, max(
-        selectedYear()$length_of_encounter_minutes
+        selectedYearHistogram()$length_of_encounter_minutes
       )))
   })
   
   
-  
+  selectedyearPie <- reactive({
+    data_set_ufo %>% filter(Year == input$year_for_pie)
+  })
   season_counts <- reactive({
-    table(selectedYear()$Season)
+    table(selectedyearPie()$Season)
   })
   
   custom_colors <- c("#f28500", "green", "#F2FD00", "#00FDF2")
@@ -41,7 +44,10 @@ server <- function(input, output) {
     pie(
       season_counts(),
       labels = names(season_counts()),
-      main = paste("Pie Chart of the repartition of the encounters of UFO by season for the year",input$years),
+      main = paste(
+        "Pie Chart of the repartition of the encounters of UFO by season for the year",
+        input$year_for_pie
+      ),
       col = custom_colors
     )
     
@@ -49,9 +55,6 @@ server <- function(input, output) {
   
   
   
-  output$columns <- renderText({
-    paste(";", print(colnames(data_set_ufo)))
-  })
   
   output$ufoMap <- renderLeaflet({
     map_data <- data_set_ufo %>%
--- ui.R
+++ ui.R
@@ -17,38 +17,56 @@ data_set_ufo <-
 
 ui <- fluidPage(
   # App title ----
-  titlePanel(h1("Dashboard to analyse the ufo sightings ", align = "center")),
+  titlePanel(h1(
+    "Dashboard to analyse the ufo sightings ", align = "center"
+  )),
   
   
-  sidebarLayout(mainPanel(plotOutput("histogram")),
-                sidebarPanel(
-                  sliderInput(
-                    inputId = "years",
-                    label = "Year",
-                    min = min(data_set_ufo$Year),
-                    max = max(data_set_ufo$Year),
-                    step = 5,
-                    value = 1960,
-                    sep = ""
-                  )
-                  
-                )),
+  mainPanel(
+    plotOutput("histogram"),
+    
+    sliderInput(
+      inputId = "years",
+      label = "Year",
+      min = min(data_set_ufo$Year),
+      max = max(data_set_ufo$Year),
+      step = 1,
+      value = 1960,
+      sep = ""
+    )
+  ),
   
-  textOutput("columns"),
-  plotOutput("pieChart"),
+  tags$br(),
+  
+  
+  mainPanel(
+    plotOutput("pieChart"),
+    sliderInput(
+      inputId = "year_for_pie",
+      label = "Year",
+      min = min(data_set_ufo$Year),
+      max = max(data_set_ufo$Year),
+      step = 1,
+      value = 1960,
+      sep = ""
+    )
+  ),
   mainPanel(tabsetPanel(type = "tabs",
                         
                         tabPanel(
                           "Map", leafletOutput("ufoMap")
-                        ))),
-  sidebarPanel(sliderInput(
-    inputId = "year_for_map",
-    label = "Year",
-    min = min(data_set_ufo$Year),
-    max = max(data_set_ufo$Year),
-    step = 5,
-    value = 1960,
-    sep = ""
-  ))
+                        )),
+            sidebarPanel(
+                          sliderInput(
+                            inputId = "year_for_map",
+                            label = "Year",
+                            min = min(data_set_ufo$Year),
+                            max = max(data_set_ufo$Year),
+                            step = 1,
+                            value = 1960,
+                            sep = ""
+                          ))
+  
+  )
   
 )
